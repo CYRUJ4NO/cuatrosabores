c86d4a3f37fea3f7b7b4248259f256d65392f6af
Jano Matus <138126250+CYRUJ4NO@users.noreply.github.com>
2023-11-30 12:48:45 -0300

    Update cuatroSensores.ino

--- cuatroSensores.ino
+++ cuatroSensores.ino
@@ -5,29 +5,33 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 ///los pines para el trabajo en efecto lm35:A1  ,hrsr04 t=40,e=41 ,boton d22 ,led d30 ,ldr A0 
 
 const int Trigger = 40;   //Pin digital 2 para el Trigger del sensor
 const int Echo = 41;   //Pin digital 3 para el Echo del sensor
-int ldr;
+int ldr; //Se conecta a la analogina A0
 int boton=22;
 int led = 30;
 int estadoled;
-int tempC;
-
+int tempC;  //Se conecta a la analogina A1
+//APLICAMOS EL RETARDO PARA CADA MUESTREO 
+int retardo=500;
 
 void setup() {
-   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-  Serial.begin(9600);//iniciailzamos la comunicación
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  delay(1000);
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  Serial.begin(115200);//iniciailzamos la comunicación
   pinMode(boton,INPUT);
   pinMode(led,OUTPUT);
   pinMode(Trigger, OUTPUT); //pin como salida
@@ -80,20 +84,52 @@ void loop()
   Serial.print("cm");
   Serial.println();
   
-  //imprime en oled
-  display.clearDisplay();
+  // imprime en oled
+  display.clearDisplay();//Limpia la pantalla
+  
+  // temperatura
   display.setTextSize(1);
   display.setCursor(0,0);
-  display.print("Temperature: ");
-  display.setTextSize(2);
-  display.setCursor(0,10);
+  display.print("Temperatura: ");
+  display.setCursor(78,00);
   display.print(tempC);
   display.print(" ");
-  display.setTextSize(1);
   display.cp437(true);
   display.write(167);
-  display.setTextSize(2);
   display.print("C");
-  
-  delay(1000);          //Hacemos una pausa de 100ms
+
+  // distancia
+  display.setTextSize(1);
+  display.setCursor(0, 10);
+  display.print("Distancia: ");
+  display.setCursor(70, 10);
+  display.print(d);
+  display.print("cm"); 
+
+  // Estado boton
+   if (estadoled){
+    display.setTextSize(1);
+    display.setCursor(0, 20);
+    display.print("Estado boton: ");
+    display.setCursor(80, 20);
+    display.print("ON");
+   }else{
+    display.setTextSize(1);
+    display.setCursor(0, 20);
+    display.print("Estado boton: ");
+    display.setCursor(80, 20);
+    display.print("OFF"); 
+    }
+
+  //LDR para display de 128x32 esta info no se ve
+    display.setTextSize(1);
+    display.setCursor(0, 30);
+    display.print("Cantidad luz: ");
+    display.setCursor(80, 30);
+    display.print(ldr);
+
+
+  //Envia info al display
+  display.display();
+  delay(retardo);          //Hacemos una pausa
 }
